55ac42b32cb38c071cda83c62b2d53d0f5e16060
serpient <littlegangwolf@gmail.com>
2019-01-18 15:13:29 -0800

    setup basic user input and handler

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    }
+  }
+  
+  handleUserInput = (e) => {
+    let { value, name } = e.currentTarget;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    let { input } = this.state;
     return (
       <div className="App">
-        
+        <h1 className='App-Title'>Let's Find Some Books!</h1>
+        <input 
+          className='query-input' 
+          type='search' 
+          name='input' 
+          value={input} 
+          placeholder='Search by book title or author...'
+          onChange={(e) => {this.handleUserInput(e)}} 
+        />
+        { input }
       </div>
     );
   }
