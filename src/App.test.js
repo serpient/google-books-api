f6d90ca794862a5dd505085a07dcae79d405d0c3
serpient <littlegangwolf@gmail.com>
2019-01-19 17:31:18 -0800

    add pre-api call unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -18,8 +18,55 @@ it('saves user input correctly in state', () => {
   expect(wrapper.state().input).toEqual('test')
 })
 
-it('makes query with correct user input', () => {
+describe('before API calls, inputis checked and state is reset', () => {
+  let renderedComponent;
+  const mockResults = { totalItems: 1, items: [{ volumeInfo: { title: 'test' }}]};
+  beforeEach(() => { 
+    renderedComponent = shallow(<App />);
+  })
+  it('resets results before querying for new data', () => {
+    renderedComponent.setState({ input: 'test', results: mockResults.items });
+    renderedComponent.instance().handleSubmitQuery();
+    expect(renderedComponent.state('results')).toEqual([])
+  })
+  
+  it('with empty input, will not query for new data', () => {
+    renderedComponent.setState({ input: '', results: mockResults.items });
+    renderedComponent.instance().handleSubmitQuery();
+    expect(renderedComponent.state('results')).toEqual(mockResults.items)
+    expect(renderedComponent.state('error')).toEqual('Please provide a search query first')
+  })
+})
 
+
+describe('api calls', () => {
+  let renderedComponent;
+  let queryFn;
+  const mockResults = { totalItems: 1, items: [{ volumeInfo: { title: 'test' }}]};
+  beforeAll(() => {
+    queryFn = jest.fn()
+      .mockImplementationOnce(() => ({
+        status: 200,
+        json: () => new Promise((resolve,reject) => {
+          resolve(mockResults)
+        })
+      }))
+      .mockImplementationOnce(() => ({
+        status: 500,
+      }))
+    jest.mock(queryFn);
+  })
+  beforeEach(() => { 
+    renderedComponent = shallow(<App />);
+  })
+  it('saves results to state after making successful query', () => {
+    renderedComponent.instance().queryAPI('test');
+    expect(renderedComponent.state('results')).toEqual(mockResults.items);
+  })
+  it('sets error after failed query', () => {
+    renderedComponent.instance().queryAPI('test');
+    expect(renderedComponent.state('error')).not.toEqual(false);
+  })
 })
 
 it('renders book cards when results is not empty', () => {
